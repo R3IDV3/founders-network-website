a69ea18b28a1d0a3a6118076a86a9b76bfddd4be
David <dnisenbaum1@gmail.com>
2016-07-10 00:45:48 -0400

    added page for education, updated routing and nav for it, and added a bg pic. Also added favicon

---
+++ education.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Western Founders Network - About Us</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+	</head>
+	<body id="education">
+		<?php include 'inc/assets.php' ?>
+		<?php include 'inc/ip-header.php' ?>
+		<?php include 'inc/nav-portrait.php' ?>
+		<div class="ip-main">
+			<header>
+				<section class="site-section-heading -education">
+					<div class="ac">
+						<h1 class="small-caps site-section-title">Education &amp; Projects Portfolio</h1>
+						<p class="serif"><em>Bonding over technology breeds innovation and creates partnerships that can last forever. Founders Network is commited to create a collaborative environment where student can build, break, and most importantly, learn together.</em></p>
+					</div>
+				</section>
+				<?php include 'inc/nav.php' ?>
+			</header>
+
+			<?php include 'inc/footer.php' ?>
+		</div><!-- .ip-main -->
+		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script src="dist/jquery.easing.1.3.js"></script>
+		<script src="dist/fn.min.js"></script>
+	</body>
+</html>
