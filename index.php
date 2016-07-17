800948fe3cef5cdd07ca6889004fe389330b9ec9
Reid Vender <reid.vender@me.com>
2016-07-17 15:26:53 -0400

    Added routes

--- index.php
+++ index.php
@@ -16,7 +16,9 @@
 		'/director-application-results' => 'aresults.php',
 		'/join' => 'early-bird-registration.php',
 		'/join-results' => 'eearesults.php',
-		'/education' => 'education.php'
+		'/education' => 'education.php',
+		'/entrepreneurship' => 'entrepreneurship.php',
+		'/industry' => 'industry.php'
 	));
 
 	// '/apply/education-executive' => 'education-executive-application.php',
