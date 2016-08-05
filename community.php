8ac12f42818d00e6e326f9a41ea9a6a166eb55dc
Reid Vender <reid.vender@me.com>
2016-08-04 22:03:40 -0400

    Added community page

--- community.php
+++ community.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Western Founders Network - About Us</title>
+		<title>Western Founders Network - Community</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<script src="//a.mailmunch.co/app/v1/site.js" id="mailmunch-script" data-mailmunch-site-id="244825" async="async"></script>
 	</head>
-	<body id="about">
+	<body id="community">
 		<?php include 'inc/assets.php' ?>
 		<?php include 'inc/ip-header.php' ?>
 		<?php include 'inc/nav-portrait.php' ?>
 		<div class="ip-main">
 			<header>
-				<section class="site-section-heading -about">
+				<section class="site-section-heading -community">
 					<div class="ac">
-						<h1 class="small-caps site-section-title">About Us</h1>
+						<h1 class="small-caps site-section-title">Community</h1>
 						<p class="serif"><em>Founders Network is for students, by students; bringing together resources from a number of diverse channels to help make your dreams a reality.</em></p>
 					</div>
 				</section>
@@ -21,40 +21,71 @@
 			</header>	
 			<article>
 				<header class="blurb">
-					<h1 class="small-caps">Membership Base</h1>
-					<p>Running a venture is seldom an individual pursuit, but rather a team effort. We strive to build a community of students that are just as innovative and passionate as you. We envision an environment that is diverse in background and also diverse in ideas, where members can connect with future partners from a range of different faculties. Look no further than Founders Network to assemble your very own dream team.</p>
+					<h1 class="small-caps">Community</h1>
+					<p>Founders Network was created with the belief that innovation is not restricted to any particular subset of the population. We believe that all students deserve the opportunity to learn in a creative environment that encourages them to make mistakes with the ultimate goal of gaining valuable lessons along the way. With such a diverse array of members from different backgrounds and programs, Founders Network has proven to facilitate a community that introduces interesting and unique business concepts.</p>
+<!--
+					<br><br>
+					<h1 class="small-caps">For Students, By Students</h1>
+					<p style="width: 55vw;">Our membership base is made up of a unique blend of backgrounds, which facilitates specialization and invigorates problem solving amongst entrepreneurially-minded individuals.</p>
+-->
 				</header>
+			</article>
+			<article>
+    			<section class="stats">
+					<div class="stat">
+						<i class="fa fa-users"></i>
+						<div class="count fit"><span class="n">200</span>+</div>
+						<h1 class="small-caps fit">Members</h1>
+					</div><div class="stat">
+						<i class="fa fa-university"></i>
+						<div class="count fit" ><span class="n">3</span></div>
+						<h1 class="small-caps fit">Western Campuses</h1>
+					</div><div class="stat">
+						<i class="fa fa-globe"></i>
+						<div class="count fit"><span class="n">4</span></div>
+						<h1 class="small-caps fit">Years of Study</h1>
+					</div><div class="stat">
+						<i class="fa fa-graduation-cap"></i>
+						<div class="count fit"><span class="n">13</span></div>
+						<h1 class="small-caps fit">Majors Represented</h1>
+					</div>
+				</section>
+				<header class="blurb">
+					<h1 class="small-caps">For Students, By Students</h1>
+					<p style="width: 55vw;">Our membership base is made up of a unique blend of backgrounds, which facilitates specialization and invigorates problem solving amongst entrepreneurially-minded individuals.</p>
+				</header>
+<!--
 				<section class="stats">
 					<div class="stat">
-<!-- 						<i class="fa">1</i> -->
 						<div class="count fit"><span class="n">53</span>%</div>
 						<h1 class="small-caps fit">Year <span class="char">1</span></h1>
 					</div><div class="stat">
-<!-- 						<i class="fa">2</i> -->
 						<div class="count fit"><span class="n">29</span>%</div>
 						<h1 class="small-caps fit">Year <span class="char">2</span></h1>
 					</div><div class="stat">
-<!-- 						<i class="fa">3</i> -->
 						<div class="count fit" ><span class="n">8</span>%</div>
 						<h1 class="small-caps fit">Year <span class="char">3</span></h1>
 					</div><div class="stat">
-<!-- 						<i class="fa">4</i> -->
 						<div class="count fit"><span class="n">10</span>%</div>
 						<h1 class="small-caps fit">Year <span class="char">4</span></h1>
 					</div>
 				</section>
+-->
 			</article>
 			<article class="testimonial -member-base">
 				<div class="ac">
 					<p class="serif"><cite>We strive to build a community of students that are just as innovative and passionate as you</cite></p>
+<!--
 					<p class="attribution"><em>Justin Tang</em></p>
 					<p class="small-caps attributor-title">Chairman, Western Founders Network</p>
+-->
 				</div>
 			</article>
-				<header class="blurb">
-					<h1 class="small-caps">For Students, By Students</h1>
-					<p style="width: 55vw;">Founders Network was created with the belief that innovation is not restricted to any educational degree. Our membership base comprises students from all subsets of the Western population. We have the belief that a team with a strong interdisciplinary academic background will be better equipped to handle the challenges of the future. In order to foster innovation at Western, we strive to facilitate an entrepreneurial atmosphere on campus, build a community of hard working students, and to bring together Founders that are as diverse in ideas as they are in background.</p>
-				</header>
+			<article>
+    			<header class="blurb">
+					<h1 class="small-caps">Community is being part of something bigger than yourself</h1>
+					<p style="width: 55vw;">As a community invested in growth and development, we are committed to building to building bridges between the students at Western University and their startup ideas through robust outreach and and engaging educational activities that are unparalleled to a university club in Canada.</p>
+				</header>	
 				<section class="stats five">
 					<div class="stat">
 						<i class="fa fa-balance-scale"></i>
@@ -78,9 +109,12 @@
 						<h1 class="small-caps fit">Engineering</h1>
 					</div>
 				</section>
-			<article>
-				
-			</article>
+            </article>
+            <article>
+                <section class="feature-btn">
+                    <a href="/join" class="btn small-caps">Become a Member of Our Community</a>
+                </section>
+            </article>
 			<?php include 'inc/footer.php' ?>
 		</div><!-- .ip-main -->
 		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
--- js/active-nav.js
+++ js/active-nav.js
@@ -1,8 +1,6 @@
 $(function() {
 	$('#home').find('nav').find('a:contains("Home")').parent().addClass("active");
-	$('#about').find('nav').find('a:contains("About")').parent().addClass("active");
-	$('#team').find('nav').find('a:contains("Team")').parent().addClass("active");
-	$('#programs').find('nav').find('a:contains("Programs")').parent().addClass("active");
+	$('#community').find('nav').find('a:contains("Community")').parent().addClass("active");
 	$('#events').find('nav').find('a:contains("Events")').parent().addClass("active");
 	$('#faq').find('nav').find('a:contains("FAQ")').parent().addClass("active");
 });
