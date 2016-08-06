854cfee918ab30f6dd7f56d8760e567198908ce0
Reid Vender <reid.vender@me.com>
2016-08-05 20:53:55 -0400

    Updated homepage

--- home.php
+++ home.php
@@ -56,17 +56,17 @@
 						<a class="diamond-icon" title="Learn More" href="/education"><i class="fa fa-code"></i></a>
 						<h1 class="small-caps">Education</h1>
 						<p>Monthly Seminars &amp; Workshops, Development Projects, Weekly Project Meetups &amp; Scrum, Github Portfolio, Product Showcase</p>
-						<a href="/education"> Learn More </a>
+						<a class="btn small-caps" href="/education"> Learn More </a>
 					</div><div class="program">
 						<a class="diamond-icon" title="Learn More" href="/entrepreneurship"><i class="fa fa-lightbulb-o"></i></a>
 						<h1 class="small-caps">Entrepreneurship</h1>
 						<p>Socials, Global Entrepreneurship Week, Ideas Workshops, Future View Conference, Entrepreneurial Speaker Series, Technology Showcase, Pitch Competitions</p>
-						<a href="/entrepreneurship"> Learn More </a>
+						<a class="btn small-caps" href="/entrepreneurship"> Learn More </a>
 					</div><div class="program">
 						<a class="diamond-icon" title="Learn More" href="/industry"><i class="fa fa-pencil"></i></a>
 						<h1 class="small-caps">Industry</h1>
 						<p>Case Competitions, Resume Workshops, Career Panels, Kickstarter Workshop, Career Fairs, Techlink, Algorithms Practice</p>
-						<a href="/industry"> Learn More </a>
+						<a class="btn small-caps" href="/industry"> Learn More </a>
 					</div>
 				</section>
 			</article>
@@ -95,6 +95,7 @@
     				</div>
 					<p>Founders Network has recently partnered with Toronto technology and design incubator Tract9, come check out their <a href="http://tract9.com/toronto-events/tract-9-youth-innovation-booster/" target="_blank">Youth Innovation Booster</a> this weekend. This a perfect opportunity to advance any budding entrepreneurship aspirations or projects as you will get access to top notch experts in venture capital and entrepreneurship!</p>
 				</header>
+<!--
 				<header class="blurb">
 					<h1 class="small-caps">Affiliates <span class="char">&</span> Partners</h1>
 					<p>All of our affiliates and partners are satisfied with our organization. Hopefully your company is next.</p>
@@ -116,6 +117,33 @@
 						</div>
 					</div>
 				</article>
+-->
+			</article>
+			<article class="inverted">
+    			<header class="blurb wide">
+					<p class="small-caps" style="font-size: large;">Founders Network is for students, by students. We recognize that our Founders will come from a set of diverse backgrounds with varied skills which is why we have constructed three specially designed pillars to encompass the entire spectrum of talent that we attract.</p>
+				</header>
+				<header class="blurb wide">
+    				<h1 class="small-caps">Education</h1>
+					<p>At Founder’s Network, we believe programming literacy is becoming ever more essential regardless of one’s industry or career. Whether you are a computer science student looking to build up your github portfolio by contributing to projects, or a business student looking to gain basic programming skills, we’ve got you covered. Under this portfolio you will work with experienced project leaders who are upper year students with solid industry experience leading software development projects.</p>
+				</header>
+				<header class="blurb wide">
+    				<h1 class="small-caps">Industry</h1>
+					<p>The Industry Group within Founders Network aims to get our members in touch with real world hiring experiences. Through events and engagements with industry professionals, you will receive much needed exposure to the competitive environment that will soon become your environment and can begin to sharpen those interpersonal skills necessary to navigate the social jungle that is otherwise known as recruiting.</p>
+				</header>
+				<header class="blurb wide">
+    				<h1 class="small-caps">Entrepreneurship</h1>
+					<p>Entrepreneurs are risk takers, leaders, innovators, and above all irrefutably persistent hard workers. Here at Founders Network we believe all students have the potential to become an entrepreneur but not all students have access to the resources necessary to begin. We envision a dynamic and balanced entrepreneurial ecosystem for you to thrive in and eventually evolve from. The events under the Entrepreneurship Portfolio have been tailored to amplify this vision.</p>
+				</header>
+			</article>
+			<article>
+				<header class="blurb wide">
+					<h1 class="small-caps">Get Involved</h1>
+					<p>Feel free to drop in on our project based, monthly programming seminars, which allow students to get involved with web/app development – or grab a group of buddies and attend a members’ social to network with fellow business junkies and industry veterans. From our career panels and resume workshops, to the annual Microsoft Technology Consulting Case Competition on campus, Founders Network attracts over 350 members representing every faculty at Western and serves as the ideal destination for any student focused on a future in tech, entrepreneurship, or consulting. Sign up for the mailing list today and get the registration details in September.</p>
+				</header>
+				<section class="feature-btn">
+                    <a href="/join" class="btn small-caps">Become a Member of Our Community</a>
+                </section>
 			</article>
 			<?php include 'inc/footer.php' ?>
 		</div><!-- .ip-main -->
