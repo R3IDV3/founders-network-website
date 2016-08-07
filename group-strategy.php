50e515ac5cafcb756848b66daddf5fc1a717cf7a
Reid Vender <reid.vender@me.com>
2016-08-07 13:25:24 -0400

    Added group strategy page

---
+++ group-strategy.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Western Founders Network - Group Strategy</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script src="//a.mailmunch.co/app/v1/site.js" id="mailmunch-script" data-mailmunch-site-id="244825" async="async"></script>
+	</head>
+	<body id="group-strategy">
+		<?php include 'inc/assets.php' ?>
+		<?php include 'inc/ip-header.php' ?>
+		<?php include 'inc/nav-portrait.php' ?>
+		<div class="ip-main">
+			<header>
+				<section class="site-section-heading -group-strategy">
+					<div class="ac">
+						<h1 class="small-caps site-section-title">Group Strategy</h1>
+					</div>
+				</section>
+				<?php include 'inc/nav.php' ?>
+			</header>	
+			<article>
+				<header class="blurb">
+					<h1 class="small-caps">Our Mandate</h1>
+					<p>To bring together entrepreneurially minded individuals from diverse academic and social backgrounds, develop interest in business and technology, and promote programming literacy for the future leaders of our community.</p>
+				</header>
+			</article>
+			<article class="inverted">
+    			<header class="blurb wide">
+					<h1 class="small-caps">Three Pillars Foundation</h1>
+    				<p>Western Founders Network is built upon three pillars designed to complement one another in order to prepare you for a career in technology, consulting, product management, or entrepreneurship.
+						<br>
+						<br>
+						While our education portfolio aims at helping members hone in their technical skills with projects and seminars, our industry portfolio focuses on getting our members involved with the industry through consulting case competitions & career panels. They work in conjunction with the entrepreneurship group which connects our members with successful entrepreneurs from Western and beyond.
+    				</p>
+				</header>
+				<section class="feature-btn thirds">
+    				<a class="btn small-caps" href="/education">Education</a><a class="btn small-caps" href="/entrepreneurship">Entrepreneurship</a><a class="btn small-caps" href="/industry">Industry</a>
+				</section>
+			</article>
+			<article>
+    			<header class="blurb wide">
+					<h1 class="small-caps">Focus on Community</h1>
+    				<p>Founders Network is more than just a club. It is a community of ambitious and entrepreneurially minded individuals gathered together to connect with one another, share expertise, and spark new business ideas. With us you will meet your next business partner, colleague, and even your potential housemate.</p>
+				</header>
+				<header class="blurb wide">
+					<h1 class="small-caps">Education Strategy</h1>
+    				<p>Education programs at Founders Network are project-based. That means rather than just attending seminars and workshops, you will get the opportunity to contribute to the development of an actual product.</p>
+				</header>
+            </article>
+			<?php include 'inc/footer.php' ?>
+		</div><!-- .ip-main -->
+		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script src="dist/jquery.easing.1.3.js"></script>
+		<script src="dist/fn.min.js"></script>
+	</body>
+</html>
